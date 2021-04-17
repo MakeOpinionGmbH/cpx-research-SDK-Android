d6218c56182fda7c646e820f66414dfdfc8b0fcf
Daniel Fredrich <me@danielfredrich.de>
2021-04-17 19:29:47 +0200

    Updating demo app to latest sdk version

--- app/src/main/java/com/makeopinion/cpxresearchdemo/MainActivity.kt
+++ app/src/main/java/com/makeopinion/cpxresearchdemo/MainActivity.kt
@@ -94,7 +94,7 @@ class MainActivity : FragmentActivity() {
                     "New surveys available",
                     20,
                     "#ffffff",
-                    "ffaf20",
+                    "#ffaf20",
                     true)
             it.setStyle(style)
         }
