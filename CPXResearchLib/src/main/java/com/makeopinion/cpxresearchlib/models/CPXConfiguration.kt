10d3fde14379cbc8b2805bd73ec34844e356763d
Daniel Fredrich <me@danielfredrich.de>
2021-05-25 19:48:36 +0200

    Setting correct sdk version

--- CPXResearchLib/src/main/java/com/makeopinion/cpxresearchlib/models/CPXConfiguration.kt
+++ CPXResearchLib/src/main/java/com/makeopinion/cpxresearchlib/models/CPXConfiguration.kt
@@ -42,7 +42,7 @@ class CPXConfiguration(
                         Pair("text", style.text),
                         Pair("textsize", "${style.textSize.toPx()}"),
                         Pair("sdk", "android"),
-                        Pair("sdk_version", "1.0.0"),
+                        Pair("sdk_version", "1.0.2"),
                         Pair("secure_hash", CPXHash.md5("${extUserId}-${secureHash}"))
                 )
 
