b09a5105d3596b3a3d701107f092b4d1f31fec0b
Daniel Fredrich <me@danielfredrich.de>
2021-11-01 13:38:11 +0100

    Updated demo app to latest version of SDK, added Java demo

---
+++ Javademo/app/src/main/java/com/makeopinion/javademo/CPXApplication.java
@@ -0,0 +1,44 @@
+package com.makeopinion.javademo;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
+import com.makeopinion.cpxresearchlib.CPXResearch;
+import com.makeopinion.cpxresearchlib.models.CPXConfiguration;
+import com.makeopinion.cpxresearchlib.models.CPXConfigurationBuilder;
+import com.makeopinion.cpxresearchlib.models.CPXStyleConfiguration;
+import com.makeopinion.cpxresearchlib.models.SurveyPosition;
+
+public class CPXApplication extends Application {
+
+    private CPXResearch cpxResearch;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initCPX();
+    }
+
+    @NonNull
+    public CPXResearch getCpxResearch() {
+        return cpxResearch;
+    }
+
+    private void initCPX() {
+        CPXStyleConfiguration style = new CPXStyleConfiguration(SurveyPosition.CornerBottomRight,
+                "Earn up to<br> 3 Coins",
+                20,
+                "#ffffff",
+                "#ffaf20",
+                true);
+
+        CPXConfiguration config = new CPXConfigurationBuilder("<your app id>",
+                "<extUserId>",
+                "<your secure hash>",
+                style)
+                .build();
+
+        cpxResearch = CPXResearch.Companion.init(config);
+    }
+}
---
+++ Javademo/app/src/main/java/com/makeopinion/javademo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.makeopinion.javademo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.makeopinion.cpxresearchlib.CPXResearchListener;
+import com.makeopinion.cpxresearchlib.models.CPXCardConfiguration;
+import com.makeopinion.cpxresearchlib.models.TransactionItem;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements CPXResearchListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewGroup parentView = findViewById(R.id.ll_container);
+
+        CPXApplication app = (CPXApplication) getApplication();
+        app.getCpxResearch().registerListener(this);
+        app.getCpxResearch().setSurveyVisibleIfAvailable(true, this);
+
+        CPXCardConfiguration cardConfig = new CPXCardConfiguration(
+                Color.parseColor("#41d7e5"),
+                Color.WHITE,
+                Color.parseColor("#dfdfdf"),
+                Color.parseColor("#ffaa00"),
+                Color.DKGRAY,
+                3);
+
+        app.getCpxResearch().insertCPXResearchCardsIntoContainer(this, parentView, cardConfig);
+    }
+
+    @Override
+    public void onSurveysDidClose() {
+        Log.d("CPXDEMO", "Surveys did close.");
+    }
+
+    @Override
+    public void onSurveysDidOpen() {
+        Log.d("CPXDEMO", "Surveys did open.");
+    }
+
+    @Override
+    public void onSurveysUpdated() {
+        Log.d("CPXDEMO", "Surveys updated.");
+    }
+
+    @Override
+    public void onTransactionsUpdated(List<TransactionItem> unpaidTransactions) {
+        Log.d("CPXDEMO", String.format("Transactions updated with %d items", unpaidTransactions.size()));
+    }
+}
